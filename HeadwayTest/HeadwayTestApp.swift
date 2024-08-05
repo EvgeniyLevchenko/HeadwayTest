e1b85442d94ae57d7dea29c539cd37dfec9b988f
ye.levchenko <ye.levchenko@andersenlab.com>
2024-08-05 13:10:18 +0300

    Initial Commit

---
+++ HeadwayTest/HeadwayTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadwayTestApp.swift
+//  HeadwayTest
+//
+//  Created by Yevhenii Levchenko on 05.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct HeadwayTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
